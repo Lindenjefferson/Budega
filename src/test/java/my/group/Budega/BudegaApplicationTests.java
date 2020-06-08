22e03e905bfca52ff8107c292af57f941683f837
Lindenjefferson <lindenjeffersom@hotmail.com>
2020-06-08 16:32:26 -0300

    Initial commit

---
+++ src/test/java/my/group/Budega/BudegaApplicationTests.java
@@ -0,0 +1,13 @@
+package my.group.Budega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
