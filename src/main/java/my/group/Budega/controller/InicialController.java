28637be819d8f64dac800edd8fd7cf31c1daf00a
Lindenjefferson <lindenjeffersom@hotmail.com>
2020-07-04 22:16:11 -0300

    Redirecionamento inicial

---
+++ src/main/java/my/group/Budega/controller/InicialController.java
@@ -0,0 +1,16 @@
+package my.group.Budega.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class InicialController {    
+    
+    @GetMapping
+    public String inicio() {
+        return "redirect:produto";
+    }
+
+}
